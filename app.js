1db59f26bc6ade930b4d1481368bc06fa8fe62f3
Bill42362 <bill42362@gmail.com>
2016-05-14 23:25:05 +0800

    Enable serving static files with `Inert`.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Debug = require('debug');
 const Fs = require('fs');
 const Hapi = require('hapi');
 const Http2 = require('http2');
+const Inert = require('inert');
 
 const server = new Hapi.Server();
 
@@ -10,12 +11,24 @@ let listener = Http2.createServer({
     key: Fs.readFileSync('./ssl/localhost.key'),
     cert: Fs.readFileSync('./ssl/localhost.crt')
 });
-
 listener.address = listener.address || function() { return this._server.address() }
 
 server.connection({listener: listener, port: '3000', tls: true});
+server.register(Inert, () => {});
+
 server.route({
-    method: 'get', path: '/', handler: (request, reply) => reply('hello world')
+    method: 'get', path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'dist/html',
+            redirectToSlash: true,
+            index: ['index.js'],
+        }
+    }
+});
+server.route({
+    method: 'get', path: '/js/{param*}',
+    handler: { directory: { path: 'dist/js', } }
 });
 
 server.start(err => {
